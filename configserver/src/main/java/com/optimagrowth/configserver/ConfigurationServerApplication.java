726849d9465650800f1b60f5c65dc315c7dd29b6
nicehcy <nicehcy2@naver.com>
2025-07-09 15:17:23 +0900

    5-2장

--- configserver/src/main/java/com/optimagrowth/configserver/ConfigurationServerApplication.java
+++ configserver/src/main/java/com/optimagrowth/configserver/ConfigurationServerApplication.java
@@ -2,8 +2,10 @@ package com.optimagrowth.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer // 이 서비스를 스프링 클라우드 컨피그 서비스로 활성화한다.
 public class ConfigurationServerApplication {
 
 	public static void main(String[] args) {
