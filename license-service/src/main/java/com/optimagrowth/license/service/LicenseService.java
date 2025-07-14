d08256a92ab776424876f9ecea0a9f6d5926e23d
nicehcy <nicehcy2@naver.com>
2025-07-14 23:36:37 +0900

    license 디렉토리 이름 변경

---
+++ license-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -0,0 +1,56 @@
+package com.optimagrowth.license.service;
+
+import com.optimagrowth.license.config.ServiceConfig;
+import com.optimagrowth.license.model.License;
+import com.optimagrowth.license.repository.LicenseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Locale;
+import java.util.Random;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class LicenseService {
+
+    private final MessageSource messages;
+    private final LicenseRepository licenseRepository;
+    private final ServiceConfig config;
+
+    public License getLicense(String licenseId, String organizationId) {
+
+        License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
+        if (license == null) {
+            throw new IllegalArgumentException(
+                    String.format(messages.getMessage(
+                            "license.search.error.message", null, null),
+                            licenseId, organizationId));
+        }
+        return license.withComment(config.getProperty());
+    }
+
+    public License createLicense(License license) {
+
+        license.setLicenseId(UUID.randomUUID().toString());
+        licenseRepository.save(license);
+        return license.withComment(config.getProperty());
+    }
+
+    public License updateLicense(License license) {
+
+        licenseRepository.save(license);
+        return license.withComment(config.getProperty());
+    }
+
+    public String deleteLicense(String licenseId) {
+
+        License license = new License();
+        license.setLicenseId(licenseId);
+        licenseRepository.delete(license);
+
+        return String.format(messages.getMessage("license.delete.message", null, null),licenseId);
+    }
+}
