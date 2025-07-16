8bbb59a4603a53fff27dd7735aa1af55cee69d81
nicehcy <nicehcy2@naver.com>
2025-07-17 00:17:36 +0900

    8장

--- license-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ license-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -1,5 +1,6 @@
 package com.optimagrowth.license;
 
+import com.optimagrowth.license.utils.UserContextInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +9,9 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
 @RefreshScope
@@ -22,6 +26,17 @@ public class LicenseServiceApplication {
 	@LoadBalanced
 	@Bean
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+
+		RestTemplate template = new RestTemplate();
+		List interceptors = template.getInterceptors();
+		if (interceptors==null){
+			template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+		}
+		else{
+			interceptors.add(new UserContextInterceptor());
+			template.setInterceptors(interceptors);
+		}
+
+		return template;
 	}
 }
